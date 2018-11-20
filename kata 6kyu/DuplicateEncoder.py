b786bf7a018d12a9bc001069d9e2d0f5db97c141
Sandra <sandracabreragarcia26@gmail.com>
2018-11-20 19:23:30 +0100

    caso test con assert

--- kata 6kyu/DuplicateEncoder.py
+++ kata 6kyu/DuplicateEncoder.py
@@ -1,6 +1,50 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #caso test
-Test.assert_equals(duplicate_encode("din"),"(((")
-Test.assert_equals(duplicate_encode("recede"),"()()()")
-Test.assert_equals(duplicate_encode("Success"),")())())","should ignore case")
-Test.assert_equals(duplicate_encode("(( @"),"))((")
+#Test.assert_equals(duplicate_encode("din"),"(((")
+#Test.assert_equals(duplicate_encode("recede"),"()()()")
+#Test.assert_equals(duplicate_encode("Success"),")())())","should ignore case")
+#Test.assert_equals(duplicate_encode("(( @"),"))((")
+
+
+word =  din
+if duplicate_encode(word):
+    print("el caso test din esta correcto")
+else:
+    print("din esta to maaaaaal")
+
+word =  recede
+if duplicate_encode(word):
+    print("el caso test recede esta correcto")
+else:
+    print("recede esta to maaaaaal")
+
+word =  Success
+if duplicate_encode(word):
+    print("el caso test Success esta correcto")
+else:
+    print("Success esta to maaaaaal")
+
+word = (( @
+if duplicate_encode(word):
+    print("el caso test (( @ esta correcto")
+else:
+    print("(( @ esta to maaaaaal")
