947462acc3318f36638031b51db3b025bbfa02f0
Sandra <sandracabreragarcia26@gmail.com>
2018-11-20 18:18:04 +0100

    caso test

---
+++ kata 6kyu/DuplicateEncoder.py
@@ -0,0 +1,6 @@
+
+#caso test
+Test.assert_equals(duplicate_encode("din"),"(((")
+Test.assert_equals(duplicate_encode("recede"),"()()()")
+Test.assert_equals(duplicate_encode("Success"),")())())","should ignore case")
+Test.assert_equals(duplicate_encode("(( @"),"))((")
