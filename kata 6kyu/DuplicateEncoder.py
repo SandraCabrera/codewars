fa0fa1de04b34f87cbae762eee6040c2edcae1dd
Sandra <sandracabreragarcia26@gmail.com>
2018-11-20 19:30:00 +0100

    codigo resuelto

--- kata 6kyu/DuplicateEncoder.py
+++ kata 6kyu/DuplicateEncoder.py
@@ -1,50 +1,31 @@
+def duplicate_encode(word):
+    string = word.casefold()
+    newString = ''
+    auxString= ''
+    repeatedCharacters = ''
+    for character in string:
+            if character not in auxString:
+                auxString = auxString + character
+            else:
+                repeatedCharacters = repeatedCharacters + character
+    for position in string:
+        if position not in repeatedCharacters:
+            newString = newString + '('
+        else:
+            newString = newString + ')'
+    return newString
 
 
 
+if __name__ == '__main__':
 
+    #caso test
+    #Test.assert_equals(duplicate_encode("din"),"(((")
+    #Test.assert_equals(duplicate_encode("recede"),"()()()")
+    #Test.assert_equals(duplicate_encode("Success"),")())())","should ignore case")
+    #Test.assert_equals(duplicate_encode("(( @"),"))((")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#caso test
-#Test.assert_equals(duplicate_encode("din"),"(((")
-#Test.assert_equals(duplicate_encode("recede"),"()()()")
-#Test.assert_equals(duplicate_encode("Success"),")())())","should ignore case")
-#Test.assert_equals(duplicate_encode("(( @"),"))((")
-
-
-word =  din
-if duplicate_encode(word):
-    print("el caso test din esta correcto")
-else:
-    print("din esta to maaaaaal")
-
-word =  recede
-if duplicate_encode(word):
-    print("el caso test recede esta correcto")
-else:
-    print("recede esta to maaaaaal")
-
-word =  Success
-if duplicate_encode(word):
-    print("el caso test Success esta correcto")
-else:
-    print("Success esta to maaaaaal")
-
-word = (( @
-if duplicate_encode(word):
-    print("el caso test (( @ esta correcto")
-else:
-    print("(( @ esta to maaaaaal")
+    assert (duplicate_encode("din") == "(((")
+    assert (duplicate_encode("recede") == "()()()")
+    assert (duplicate_encode("Success") == ")())())")
+    assert (duplicate_encode("(( @") == "))((")
